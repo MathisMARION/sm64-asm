08ae6e0240ef718adbf15eedbe5b02f50cbe7a3a
MathisMARION <mathis.marion1@gmail.com>
2021-10-16 13:46:17 +0200

    Added double jump code

---
+++ double_jump/double_jump.asm
@@ -0,0 +1,158 @@
+; Overwrite the function try_print_debug_mario_object_info
+.orga 0x000861c0
+
+addiu sp, sp, 0xFFE8
+sw ra, 0x14(sp)
+; Call to our custom function
+jal 0x80406000
+nop
+lw ra, 0x14(sp)
+jr ra
+addiu sp, sp, 0x18
+
+.orga 0x01206000
+
+; --Function-wrapper-start--
+addiu sp, sp, 0xFFE8
+sw ra, 0x14(sp)
+; --------------------------
+
+; a0: General prefix for RAM addresses
+lui a0, 0x8034
+; t7: Address of the Mario Object struct
+; &marioState.marioObj = 0x8033B170 + 0x88 = 0x8033B1F8
+lw t7, 0xB1F8(a0)
+; t6: Mario's current state
+; &marioState.state = 0x8033B170 + 0x0C = 0x8033B17C
+lw t6, 0xB17C(a0)
+
+; Check for airborne actions
+; ACT_GROUP_MASK = 0x000001C0
+andi t0, t6, 0x01C0
+; ACT_GROUP_AIRBORNE = 0x00000080
+andi t0, t0, 0x0080
+bne t0, r0, airborne
+
+; Set Mario's shoe color to dark cyan
+lui t5, 0x8008
+lui t0, 0x0055
+ori t0, t0, 0x5500
+sw t0, 0xEC68(t5)
+sw t0, 0xEC6C(t5)
+sw t0, 0xEC70(t5)
+sw t0, 0xEC74(t5)
+
+; Save 1 to an unused field of the Mario Object struct
+; unused2 field has offset 0x210
+ori t1, r0, 1
+beq r0, r0, skip
+sb t1, 0x210(t7)
+
+airborne:
+
+; Check if midair jump is available (check curObject.unused1)
+lb t0, 0x210(t7)
+beq t0, r0, skip
+
+; Set Mario's shoe color to dark cyan
+lui t5, 0x8008
+lui t0, 0x0055
+ori t0, t0, 0x5500
+sw t0, 0xEC68(t5)
+sw t0, 0xEC6C(t5)
+sw t0, 0xEC70(t5)
+sw t0, 0xEC74(t5)
+
+; Check if state is ACT_TRIPLE_JUMP (0x01000882)
+lui t0, 0x0100
+addiu t0, t0, 0x0882
+beq t0, t6, checkA
+; Check if state is ACT_BACKFLIP (0x01000883)
+addiu t0, t0, 1
+beq t0, t6, checkA
+; Check if state is ACT_SIDE_FLIP (0x01000887)
+addiu t0, t0, 4
+beq t0, t6, checkA
+; Check if state is ACT_WATER_JUMP (0x01000889)
+addiu t0, t0, 2
+beq t0, t6, checkA
+; Check if state is ACT_FREEFALL (0x0100088C)
+addiu t0, t0, 3
+beq t0, t6, checkA
+; Check if state is ACT_JUMP (0x03000880)
+lui t0, 0x0300
+addiu t0, t0, 0x0880
+beq t0, t6, checkA
+; Check if state is ACT_DOUBLE_JUMP (0x03000881)
+addiu t0, t0, 1
+beq t0, t6, checkA
+; Check if state is ACT_STEEP_JUMP (0x03000885)
+addiu t0, t0, 4
+beq t0, t6, checkA
+; Check if state is ACT_WALL_KICK_AIR (0x03000886)
+addiu t0, t0, 1
+beq t0, t6, checkA
+; Check if state is ACT_LONG_JUMP (0x03000888)
+addiu t0, t0, 2
+beq t0, t6, checkA
+; Check if state is ACT_TOP_OF_POLE_JUMP (0x0300088D)
+addiu t0, t0, 5
+beq t0, t6, checkA
+; Check if state is ACT_GROUND_POUND (0x008008A9)
+lui t0, 0x0080
+addiu t0, t0, 0x08A9
+beq t0, t6, checkA
+nop
+
+beq r0, r0, skip
+nop
+
+checkA:
+
+; Set Mario's shoe color to cyan
+lui t5, 0x8008
+lui t0, 0x00FF
+ori t0, t0, 0xFF00
+sw t0, 0xEC68(t5)
+sw t0, 0xEC6C(t5)
+sw t0, 0xEC70(t5)
+sw t0, 0xEC74(t5)
+
+; Load buttons pressed on the current frame
+; &controller.buttonPressed = 0x8033AF90 + 0x12 = 0x8033AFA2
+lh t0, 0xAFA2(a0)
+
+; Check if A (0x8000) is pressed
+andi t0, t0, 0x8000
+beq t0, r0, skip
+nop
+
+; Save 0 to an unused field of the Mario Object struct
+sh r0, 0x210(t7)
+
+; Set Mario's shoe color to default
+lui t5, 0x8008
+lui t0, 0x701C
+ori t0, t0, 0x0F00
+sw t0, 0xEC68(t5)
+sw t0, 0xEC6C(t5)
+sw t0, 0xEC70(t5)
+sw t0, 0xEC74(t5)
+
+; Set Mario's state to a jump
+; arg0 is a pointer to the Mario struct (0x8033B170)
+; arg1 is the state ACT_JUMP (0x03000880)
+; arg2 isn't necessery
+; call to set_mario_action (0x80252CF4)
+addiu a0, a0, 0xB170
+lui a1, 0x0300
+jal 0x80252CF4
+addiu a1, a1, 0x0880
+
+skip:
+
+; --Function-wrapper-end----
+lw ra, 0x14(sp)
+jr ra
+addiu sp, sp, 0x18
+; --------------------------
---
+++ double_jump/double_jump_crystal.asm
@@ -0,0 +1,122 @@
+.orga 0x0120614C
+
+; --Function-wrapper-start--
+addiu sp, sp, 0xFFE8
+sw ra, 0x14(sp)
+sw s0, 0x10(sp)
+sw s1, 0x0C(sp)
+; --------------------------
+
+; s0: Address of the Mario Object struct
+; &marioState.marioObj = 0x8033B170 + 0x88 = 0x8033B1F8
+lui s0, 0x8034
+lw s0, 0xB1F8(s0)
+; s1: Address of the current Object struct
+; stored at 0x80361160
+lui s1, 0x8036
+lw s1, 0x1160(s1)
+
+; Check if object is active
+; unused2 field has offset 0x210
+lw t0, 0x210(s1)
+beq t0, r0, active
+
+; Spawn particles every 4 frames
+; spawn_sparkle_particles(1, 1, 1, -60)
+andi t1, t0, 0x0003
+bne t1, r0, noParticles
+ori a0, r0, 1
+ori a1, r0, 1
+ori a2, r0, 1
+jal 0x802B2BC8
+ori a3, r0, -60
+
+noParticles:
+
+; Decrement counter
+lw t0, 0x210(s1)
+addi t0, t0, 0xFFFF
+bne t0, r0, noCollision
+sw t0, 0x210(s1)
+
+; Enable object rendering if object becomes active
+; curObj.header.gfx.node.flags |= GRAPH_RENDER_ACTIVE
+; flags offset relative to curObj = 0x02
+; GRAPH_RENDER_ACTIVE = 0x0001
+lh t0, 0x02(s1)
+ori t0, t0, 0x0001
+sh t0, 0x02(s1)
+
+beq r0, r0, noCollision
+
+active:
+
+; Check for collision
+; arg1 is a pointer to the Mario Object struct
+; arg2 is a pointer to the current object (stored at 0x80361160)
+ori a0, s1, 0
+jal 0x802A1424
+ori a1, s0, 0
+beq v0, r0, noCollision
+nop
+
+; Save 1 to an unused field of the Mario Object struct
+; unused2 field has offset 0x210
+ori t1, r0, 1
+sb t1, 0x210(s0)
+
+; Set Mario in the freefall state
+; arg0 is a pointer to the Mario struct (0x8033B170)
+; arg1 is the state ACT_FREEFALL (0x0100088C)
+; arg2 isn't necessery
+; call to set_mario_action (0x80252CF4)
+lui a0, 0x8034
+addiu a0, a0, 0xB170
+lui a1, 0x0100
+jal 0x80252CF4
+addiu a1, a1, 0x088C
+
+; Set Mario's animation to double jump
+; arg0 is a pointer to the Mario struct (0x8033B170)
+; arg1 is the annimation ID (MARIO_ANIM_GENERAL_FALL = 0x56)
+lui a0, 0x8034
+addiu a0, a0, 0xB170
+jal 0x802509B8
+ori a1, r0, 0x56
+
+; Disable object rendering
+; curObj.header.gfx.node.flags &= ~GRAPH_RENDER_ACTIVE
+; flags offset relative to curObj = 0x02
+; GRAPH_RENDER_ACTIVE = 0x0001
+lh t0, 0x02(s1)
+andi t0, t0, 0xFFFE
+sh t0, 0x02(s1)
+
+; Set counter to 0x3C = 60 frames = 2s
+ori t0, r0, 0x003C
+sw t0, 0x210(s1)
+
+; Play sound
+; arg0 is the sound ID (SOUND_GENERAL_WALL_EXPLOSION = 0x500CA080)
+; Call to cur_obj_play_sound_2 (0x802CA1E0)
+lui a0, 0x500D
+jal 0x802CA1E0
+addiu a0, a0, 0xA080
+
+noCollision:
+
+; Load current faceYaw
+; &curObject.oFaceAngleYaw = 0x80361160 + 0x118 = 0x80361278
+;lw t0, 0x1278(t7)
+; Add 0xFF00
+;addiu t0, t0, 0xFF00
+; Store back the value
+;sw t0, 0x1278(t7)
+
+; --Function-wrapper-end----
+lw s1, 0x0C(sp)
+lw s0, 0x10(sp)
+lw ra, 0x14(sp)
+jr ra
+addiu sp, sp, 0x18
+; --------------------------
